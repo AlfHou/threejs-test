decaed72c12cfa97aaaeb08208243f1f5e9cbd91
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2020-02-03 22:09:16 +0100

    Start making solar system

--- src/entry.js
+++ src/entry.js
@@ -1,39 +1,56 @@
 import * as THREE from "three";
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 50, 0);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
+
+
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshPhongMaterial({ color: 0x00055, shininess: 60 });
-let cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const objects = [];
 
-let light = new THREE.DirectionalLight("0xFFFFFF", 1);
-light.position.set(-1, 2, 4);
+const radius = 1;
+const widthSegments = 6;
+const heightSegments = 6;
+const sphereGeometry = new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments);
+
+const solarSystem = new THREE.Object3D();
+scene.add(solarSystem);
+objects.push(solarSystem);
+
+const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
+const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+sunMesh.scale.set(5, 5, 5);
+
+solarSystem.add(sunMesh);
+objects.push(sunMesh);
+
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.PointLight(color, intensity);
+
 scene.add(light);
 
-camera.position.z = 5;
+const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+earthMesh.position.x = 10;
 
-let movement = 0.01;
+solarSystem.add(earthMesh);
+objects.push(earthMesh);
 
-animate();
+function animate(time) {
+    time *= 0.001;
 
-function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.05;
-    cube.position.z += movement;
-
-    if (cube.position.z > 3.5) {
-        movement = -0.01;
-    }
-    if (cube.position.z < -1) {
-        movement = 0.01;
-    }
-
+    objects.forEach((obj) => {
+        obj.rotation.y = time;
+    });
     renderer.render(scene, camera);
 }
+
+animate();
